3a75918e988e4b21104159e24e57ce468a8e18d9
William <williampv@gmail.com>
2023-02-07 15:15:42 -0500

    quotes update

--- src/components/citas.js
+++ src/components/citas.js
@@ -1,5 +1,18 @@
+import {useEffect, useState} from "react";
+
 function Citas() {
-    return(<div>Citas</div>)
+    const [quote, setQuote] = useState('null')
+    useEffect(function getQuotes() {
+        fetch('https://officeapi.dev/api/quotes/random')
+            .then(response => response.json())
+            .then(data => setQuote(data.data))
+    }, [])
+
+    return(
+        <div>
+            {quote?.content}
+        </div>
+    )
 }
 
 export default Citas
