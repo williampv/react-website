63cd11a5c84cd1e01c12c67f38a66ac9b7b81691
William <williampv@gmail.com>
2023-02-07 11:01:11 -0500

    menu component created

---
+++ src/components/menu.js
@@ -0,0 +1,31 @@
+function Menu() {
+    return (
+    <header className="App-header">
+        <nav>
+        <ul>
+            <li>
+            <a href="#">
+                Home
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Personajes
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Episodios
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Citas
+            </a>
+            </li>
+        </ul>
+        </nav>
+    </header>)
+}
+
+export default Menu
